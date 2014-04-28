9338f3a6e297e047e4999bb584291078f18de3be
gurevg3 <gurevg3@mail.ru>
2014-04-28 13:19:55 +0700

    Zaglushka

---
+++ Calculator/Calculator.Tests/BinaryOperations/AddedTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Calculator.Tests.BinaryOperations
+{
+   [TestFixture]
+    public class AddedTests
+    {
+       [Test]
+       public void AdderTest()
+       {
+           Assert.AreEqual(1,2);
+       }
+    }
+}
